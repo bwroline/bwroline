208aa2be7b003bfb1d34b9abe2b56537654cb8dc
bwroline <79435266+bwroline@users.noreply.github.com>
2021-03-20 11:57:43 +0100

    Add files via upload

---
+++ bmu.py
@@ -0,0 +1,82 @@
+#write online apss
+#To set the maxfile upload size
+from config import*
+from math import*
+import json.scanner
+import urllib.parse
+import urllib.response
+import pathlib
+import scandir
+import fileinput
+import bmu
+dict,bmu
+import array
+filter
+map
+dir()
+scandir= print,50,len('.NET.HTTPS://Bwroline.com%name%.com.Bwroline')
+listdir= scandir
+enteries= os.scandir('C:\\Users\\HP\\Desktop\\.com.Bwroline\\.Bwroline/')
+with os.scandir('C:\\Users\\HP\\Desktop\\.com.Bwroline\\.Bwroline/') as entries:
+    for entry in entries:
+        print(entry,'Input','...')
+        
+        b= 'Bwroline .COM Domain bwroline Host Server HTTPS URL b:',print('index','b',scandir)
+        shd= 'Bwroline Global Entertainment HTTP Media Shamudoents:',print('index1',scandir)
+        ghlp= 'Bwroline Legal HTTP Great-Hussein-Legal-Practitions Ghlp:',print('index2',scandir)
+        egb= 'Bwroline Church Evangelical HTTPS Evangelical Ecwabwr:',print('index3',scandir)
+        btgs= 'Bwroline Global Entertainment HTTPS Global Entertainments Btgees:',print('index4',scandir)
+
+        bt= print('\n\t\r Bwroline Table')
+        b0= print('\n|-----------------------------------------|-------------------------------------------------------|----------------------------|----------------------------------------|-------------------------------------------------------|------------------------------------------|')
+        b01= print('\n|\tName file type \t|\t bmu media file name \t|   index-php\t|\tState\t\t|\tWebsite Page url\t\t|\tindex-php\t\t|')
+        b02= print('\n|-----------------------------------------|-------------------------------------------------------|----------------------------|-----------------------------------------|-------------------------------------------------------|------------------------------------------|')
+        b0= print('\n|',type,'GET/type\t|\tname/media/name\t\t|\tindex\t|\tActive/Shared/On\t|\thttps://bwroline.com\t|\tindex\t\t|')
+        b1= print('\n|',type,'GET/type\t|\tname/media/name\t\t|\tindex1\t|\tActive/Shared/On\t|\thttp://shamudoents.com\t|\tindex1\t\t|')
+        b2= print('\n|',type,'GET/type\t|\tname/media/name\t\t|\tindex2\t|\tActive/Shared/On\t|\thttp://ghlp.com\t\t|\tindex2\t\t|')
+        b3= print('\n|',type,'GET/type\t|\tname/media/name\t\t|\tindex3\t|\tActive/Shared/On\t|\thttps://ecwabwr.com\t|\tindex3\t\t|')
+        b4= print('\n|',type,'GET/type\t|\tname/media/name\t\t|\tindex4\t|\tActive/Shared/On\t|\thttps://btgees.com\t\t|\tindex4\t\t|')
+        b03= print('\n|-----------------------------------------|-------------------------------------------------------|----------------------------|-----------------------------------------|-------------------------------------------------------|------------------------------------------|')
+#Get a location on system to store b
+
+        bMoney= ('\n',0,'<=',float,bt,b0,566556.566556)
+        b= 566556.566556,scandir
+        bMoney= 4*566556.566556
+        b= bt,b0,b01,b02,b1,b2,b3,b4,b03,'index\//.com.Bwroline/Users','index',print,len
+        print(bMoney)
+        config= '',b,[
+            'Bwroline Request access',b,'s user Bwroline https://bwroline.com',b,[
+                'b access s user shd http://shamudoents.com',b1,'b access s user ghlp http://ghlp.com',b2,'b access s user ecwabwr https://ecwabwr.com',b3,'b access s user btgs https://btgees.com',b4
+            ]
+        ]
+        index1= 'index1',shd,b
+        index2= 'index2',ghlp,b
+        index3= 'index3',egb,b
+        index4= 'index4',btgs,b
+        index= 'index',config
+        config= [b,index1,index2,index3,index4]
+
+        s= print(b,'Browser','User%Home%0%tools%Browser',0,print,'\n webpage','Bwroline','\n Signup','\n',getattr,input,len,75,str,chr,'Email**:','\n',getattr,input,len,8,str,chr,'New password**:',getattr,input,len,8,str,chr,'\n','Repeat new password Again**:')
+        l= print(b,'Browser','User%Home%0%tools%Browser',0,print,'\n webpage','Bwroline','\n Login','\n',getattr,input,len,75,str,chr,'Email**:',getattr,input,len,8,str,chr,'Password**:')
+        Username= print(None,'\n null','\n',s and '\n',l)
+        click= Username
+        Bwroline= Username
+        
+        if(index,Username or click and tap== 'index',index and b,[index,index1,index2,index3,index4]):
+            Username,print('user',int,'0','user',int,'0','Browser','Bwroline',Bwroline,'\n',Bwroline,'https://bwroline.com',b)
+            DEPOSIT= print('\t AMOUNT:',AMOUNT,'\n\t DATE:',DATE,'\n\t DEPOSITOR:',DEPOSITOR,Username,)
+        
+            PN= Username,print('\t https://bwroline.com','index-php','index'== [b0,b01,b02,b0,b1,b2,b3,b4],'\n PhoneNumber:',PhoneNumber,'\n DESC:','DEPOSIT',DEPOSIT,'\n PHONE NUMBER',PhoneNumber,'\n BALANCE:',BALANCE,':','+','AMOUNT',AMOUNT)
+        
+            JPN= Username,print('\t https://ecwabwr.com',b3== 'index3','2348064648565','\n PhoneNumber:',PhoneNumber,'\n DESC:','DEPOSIT',DEPOSIT,'\n PHONE NUMBER',PhoneNumber,'\n BALANCE:',BALANCE,':','+','AMOUNT',AMOUNT)
+        
+            F1PN= Username,print('\t https://btgees.com'== 'index4',b4,'2349067600141','\n PhoneNumber:',PhoneNumber,'\n DESC:','DEPOSIT',DEPOSIT,'\n PHONE NUMBER',PhoneNumber,'\n BALANCE:',BALANCE,':','+','AMOUNT',AMOUNT)
+        
+            APN= Username,print('\t http://ghlp.com'== 'index2',b2,'2348057302254','\n PhoneNumber:',PhoneNumber,'\n DESC:','DEPOSIT',DEPOSIT,'\n PHONE NUMBER',PhoneNumber,'\n BALANCE:',BALANCE,':','+','AMOUNT',AMOUNT)
+        
+            FPN= Username,print('\t http://shamudoents.com'== 'index1',b1,'2347057606390','\n PhoneNumber:',PhoneNumber,'\n DESC:','DEPOSIT',DEPOSIT,'\n PHONE NUMBER',PhoneNumber,'\n BALANCE:',BALANCE,':','+','AMOUNT',AMOUNT,b03)
+        
+        else:
+            'Browsers','0','Users','++','1','DataApps','Browser','user','0'
+            print('\n No way any part for the login or signup would be empty or wrong combination, check well to ensure you\'ve entered the correct email and password.')
+exit()
